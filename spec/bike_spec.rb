9394ce3a425874e12910af1b60a986b23224222a
Denise <yu.denise.d@gmail.com>
2014-09-15 18:43:07 +0100

    Refactor bike class and bike_spec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,30 +1,25 @@
-# Link to the Bike class
-
-require 'bike' 
-
-# We are describing the functionality of a specific class, Bike
+require './lib/bike' 
 
 describe Bike do
 
+	let(:the_bike) { Bike.new }
+
 	it "should not be broken after we create it" do 
-		the_bike = Bike.new # Initialize a new object of Bike class
 		expect(the_bike).not_to be_broken
 	end
 
 	it "should able to be broken" do 
-		the_bike = Bike.new
 		the_bike.break!
 		expect(the_bike).to be_broken
 	end
 
 	it "should be fixable" do
-		the_bike = Bike.new
 		the_bike.break!
 		the_bike.fix!
 		expect(the_bike).not_to be_broken
 	end
 
-	
+
 
 end
 	
