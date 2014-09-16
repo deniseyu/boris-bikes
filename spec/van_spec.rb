4f6b0a0f5993f6f29d6795312f0e05043f5769dc
Denise <yu.denise.d@gmail.com>
2014-09-16 22:14:52 +0100

    Add van ability to collect fixed bikes from garage

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,15 +4,25 @@ require './lib/bike.rb'
 
 describe Van do
 
-	it "should collect broken bikes" do
+	it "should collect broken bikes from docks" do
 		van = Van.new
 		bike = Bike.new
 		broken_bike = Bike.new.break!
 		van.dock(bike)
 		van.dock(broken_bike)
 
-		expect(van.broken_bikes).to eq [broken_bike]
-		
+		expect(van.broken_bikes).to eq [broken_bike]	
 	end
+
+	it "should collect fixed bikes from garage" do 
+		van = Van.new
+		bike = Bike.new 
+		broken_bike = Bike.new.break!
+		van.dock(bike)
+		van.dock(broken_bike)
+
+		expect(van.available_bikes).to eq [bike]
+	end
+	
 
 end
